c085e39a874ec0843dda31895cd7612ea8761f72
Benjamin Orimoloye <bjfikky@yahoo.com>
2023-12-15 02:10:57 -0500

    Remove context test

--- src/test/java/com/benorim/testsystem/TestSystemApplicationTests.java
+++ src/test/java/com/benorim/testsystem/TestSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestSystemApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
