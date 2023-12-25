1bde19d1e2c3b2963454ac04fd5b22c89e106ddf
Benjamin Orimoloye <bjfikky@yahoo.com>
2023-12-25 01:35:44 -0500

    Created GetTesTaker API

---
+++ src/main/java/com/benorim/testsystem/controller/api/response/TestTakerResponse.java
@@ -0,0 +1,4 @@
+package com.benorim.testsystem.controller.api.response;
+
+public record TestTakerResponse(Long id, String username) {
+}
--- src/main/java/com/benorim/testsystem/controller/api/v1/TestTakerApi.java
+++ src/main/java/com/benorim/testsystem/controller/api/v1/TestTakerApi.java
@@ -1,17 +1,22 @@
 package com.benorim.testsystem.controller.api.v1;
 
 import com.benorim.testsystem.controller.api.request.TestTakerRequest;
+import com.benorim.testsystem.controller.api.response.TestTakerResponse;
 import com.benorim.testsystem.entity.TestTaker;
 import com.benorim.testsystem.service.TestTakerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static com.benorim.testsystem.mapper.TestTakerMapper.mapTestTakerToTestTakerResponse;
 
 @RestController
 @RequestMapping("/api/v1/testTaker")
@@ -24,7 +29,7 @@ public class TestTakerApi {
     }
 
     @PostMapping("")
-    public ResponseEntity<Void> createTestTaker(@Valid @RequestBody TestTakerRequest testTakerRequest) {
+    public ResponseEntity<TestTakerResponse> createTestTaker(@Valid @RequestBody TestTakerRequest testTakerRequest) {
         TestTaker testTaker = testTakerService.createTestTaker(testTakerRequest.username());
 
         String uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -34,6 +39,12 @@ public class TestTakerApi {
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.LOCATION, uri);
-        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(mapTestTakerToTestTakerResponse(testTaker), headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TestTakerResponse> getTestTaker(@PathVariable Long id) {
+        TestTakerResponse response = mapTestTakerToTestTakerResponse(testTakerService.getTestTaker(id));
+        return response != null ? new ResponseEntity<>(response, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
