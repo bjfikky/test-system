98e9d226e60f6a983b3b58d8c3843f78f6512a80
Benjamin Orimoloye <bjfikky@yahoo.com>
2024-01-01 20:50:57 -0500

    Remove Test-Taker related code

--- src/main/java/com/benorim/testsystem/repository/TestTakerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.benorim.testsystem.repository;
-
-import com.benorim.testsystem.entity.TestTaker;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestTakerRepository extends ListCrudRepository<TestTaker, Long> {
-}
